c33e5cee5c1be0cf6e09cf8ef81fde44d4462715
康冰 <5731138+kang_bing@user.noreply.gitee.com>
2020-05-13 10:39:29 +0800

    first Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Map/>
+  </div>
+</template>
+
+<script>
+
+import Map from './map'
+export default {
+  name: 'app',
+  components: {
+    Map
+  },
+}
+</script>
+
+<style lang="stylus" ref="stylesheet/stylus" >
+#app{
+  width 100%
+  height 100%
+}
+</style>
